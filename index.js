b6202b266b5e96bfc0ba98e557e0f24167839005
liuzeyan99 <89683517+liuzeyan99@users.noreply.github.com>
2022-09-13 09:13:49 +0800

    feat: 测试合并分支的代码

---
+++ index.js
@@ -0,0 +1,9 @@
+/*
+ * @Author: liuzeyan99 89683517+liuzeyan99@users.noreply.github.com
+ * @Date: 2022-09-13 09:12:42
+ * @LastEditors: liuzeyan99 89683517+liuzeyan99@users.noreply.github.com
+ * @LastEditTime: 2022-09-13 09:13:16
+ * @FilePath: \10000\index.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+console.log('这是一个测试git 合并的代码');
